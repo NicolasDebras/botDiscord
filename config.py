50faf9020eacae3155789d46cd15b858d295c7f4
NicolasDebras <nicodebras17@gmail.com>
2026-03-28 13:48:57 +0100

    aaddddddddd

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ ADMIN_ROLE_NAME  = "Officer"
 # Nom exact du rôle Guild Master (accès aux commandes sensibles)
 GM_ROLE_NAME     = "Maitre de guilde"
 # Rôle minimum requis pour utiliser les commandes membres
-MEMBRE_ROLE_NAME = "Membre"1466706466051330061
+MEMBRE_ROLE_NAME = "Membre"
 
 # ── GUILD ID (sync instantanée des slash commands) ────────────────────────────
 GUILD_ID = int(os.environ["DISCORD_GUILD_ID"])
